89e7563fec8b8f9c6af1e8f817fead556635b9f4
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-06-16 09:51:51 +0100

    setup sql tables

---
+++ db/seed.rb
@@ -0,0 +1,4 @@
+require( 'pry' )
+require_relative('../models/tank.rb')
+require_relative('../models/rental.rb')
+require_relative('../models/customer.rb')
--- db/world_of_tanks.sql
+++ db/world_of_tanks.sql
@@ -1,3 +1,22 @@
-DROP TABLE Loans;
+DROP TABLE rentals;
 DROP TABLE customers;
 DROP TABLE tanks;
+
+CREATE TABLE tanks(
+  id serial4 PRIMARY KEY,
+  name varchar(255),
+  -- logo varchar(255),
+  country_of_origin varchar(255)
+);
+
+CREATE TABLE customers(
+  id serial4 PRIMARY KEY,
+  name varchar(255),
+);
+
+CREATE TABLE rentals;
+  id serial4 PRIMARY KEY,
+  rental_status varchar(255),
+  tank_id INT4 REFERENCES tanks(id),
+  customer_id INT4 REFERENCES customers(id)
+);
