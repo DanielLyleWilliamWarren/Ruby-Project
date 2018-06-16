c12fc63d16ef952783ce4078eaa205129f613134
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-06-16 10:10:44 +0100

    added customers and rentals to seed data

--- db/seed.rb
+++ db/seed.rb
@@ -9,11 +9,21 @@ tank1 = Tank.new({
   })
 
 tank2 = Tank.new({
-  "name" => "IS"
+  "name" => "IS",
   "country_of_origin" => "USSR"
   })
 
 tank3 = Tank.new({
-  "name" => "T29"
+  "name" => "T29",
   "country_of_origin" => "USA"
+  })
+
+customer1 = Customer.new({
+  "name" = "Georgy Zhukov"
+  })
+
+rental1 = Rental.new({
+  "customer_id" => customer1.id(),
+  "tank_id" => tank2.id(),
+  "rental_status" => "true"
   })
