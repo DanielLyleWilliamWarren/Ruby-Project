a869a7a868a1103eb462457e1af540965b5e5eda
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-06-16 10:02:40 +0100

    tanks added to seed data

--- db/seed.rb
+++ db/seed.rb
@@ -2,3 +2,18 @@ require( 'pry' )
 require_relative('../models/tank.rb')
 require_relative('../models/rental.rb')
 require_relative('../models/customer.rb')
+
+tank1 = Tank.new({
+  "name" => "Tiger I",
+  "country_of_origin" => "Germany"
+  })
+
+tank2 = Tank.new({
+  "name" => "IS"
+  "country_of_origin" => "USSR"
+  })
+
+tank3 = Tank.new({
+  "name" => "T29"
+  "country_of_origin" => "USA"
+  })
