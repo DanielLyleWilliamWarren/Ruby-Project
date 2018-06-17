41fe4edb1b8259291f9563a4de43d83e9fc3740d
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-06-17 10:35:42 +0100

    local host up and running, index and layout files working

--- wot_shop_controller.rb
+++ wot_shop_controller.rb
@@ -5,3 +5,9 @@ require_relative( './models/tank.rb')
 require_relative( './models/customer.rb')
 require_relative( './models/rental.rb')
 also_reload( './models/*')
+
+#INDEX
+get('/tanks') do
+  @tanks = Tank.all()
+  erb( :index )
+end
